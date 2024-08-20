2f8807d65d77fc4e62f13544b292e40068243f3f
Happy Coder <151567358+HappyCoder-2@users.noreply.github.com>
2024-08-20 05:11:17 +0200

    Add files via upload
    This code creates a stopwatch application with Start, Stop, and Reset functionalities. The HTML provides the structure, CSS styles the components with modern aesthetics, and JavaScript handles the stopwatch logic, including time calculations and display updates. The stopwatch updates in real-time, showing hours, minutes, seconds, and milliseconds, and allows users to control the timing through intuitive buttons.

---
+++ script.js
@@ -0,0 +1,48 @@
+// LESSON 57 - Project - Stopwatch Program
+
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+        if(!isRunning){
+                startTime = Date.now() - elapsedTime;
+                timer = setInterval(update, 10);
+                isRunning = true;
+        }
+}
+
+function stop(){
+        if(isRunning){
+                clearInterval(timer);
+                elapsedTime = Date.now() - startTime;
+                isRunning = false;
+        }
+}
+
+function reset(){
+        clearInterval(timer);
+        startTime = 0;
+        elapsedTime = 0;
+        isRunning = false;
+        display.textContent = "00:00:00:00"
+}
+
+function update(){
+        const currentTime = Date.now();
+        elapsedTime = currentTime - startTime;
+
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / 1000 % 60);
+        let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        miliseconds = String(miliseconds).padStart(2, "0");
+
+        display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+}
